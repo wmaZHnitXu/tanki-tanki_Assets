2e83a5bf1a5d04bbbd2ca05c78b477ba9238eda6
GoshaNumberOne <ivansemikin1041@mail.ru>
2024-02-01 01:37:21 +0300

    Update RectCollider.cs
    added fields to the RectCollider

--- Scripts/Model/RectCollider.cs
+++ Scripts/Model/RectCollider.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class RectCollider : Collider
 {
+    private float _width;
+    private float _height;
+    public float width
+    {
+        get => _width;
+        set
+        {
+            _width = value;
+        }
+    }
+    public float height
+    {
+        get => _height;
+        set
+        {
+            _height = value;
+        }
+    }
     public override float getPointDepth(Vector2 point)
     {
-        throw new System.NotImplementedException();
+        throw new System.NotImplementedException();//ne doper cho tyt 
     }
 }
