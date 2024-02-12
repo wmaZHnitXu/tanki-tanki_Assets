ed7a4aa5448e8388321c19e46b455d298d26ef7b
wmaZHnitXu <alesternest@gmail.com>
2024-02-12 19:45:58 +0300

    Damage ent + evt

--- Scripts/Model/Bullet.cs
+++ Scripts/Model/Bullet.cs
@@ -5,6 +5,7 @@ public class Bullet : Entity
     private Vector2 velocity;
     private float lifetime;
     private float maxLifetime = 2.0f;
+    private float damage = 10.0f;
     private Entity owner;
 
     public Bullet(Vector2 position, Vector2 velocity, Entity owner) {
@@ -20,14 +21,18 @@ public class Bullet : Entity
         Entity e = null;
         if ((e = level.TraceLine(owner, position, nextPos, out nextPos)) != null) {
 
-            hitFlag = true;
-            //Debug.Log((e as Tank).position);
+            if (e is DestructibleEntity) {
+                var d = e as DestructibleEntity;
+                d.Damage(damage, position);
+                Kill();
+                return;
+            }
         }
 
         position = nextPos;
         lifetime += delta;
 
-        if (lifetime > maxLifetime || hitFlag) {
+        if (lifetime > maxLifetime) {
             Kill();
         }
     }
--- Scripts/Model/DestructibleEntity.cs
+++ Scripts/Model/DestructibleEntity.cs
@@ -5,10 +5,25 @@ public abstract class DestructibleEntity : CollideableEntity
     private float _health;
     public float health {
         get => _health;
-        protected set => _health = health;
+        protected set {
+            _health = health;
+            if (health < 0f && !isDead) {
+                Kill();
+            }
+        }
     }
 
-    public abstract void Damage(float damage);
+    public delegate void OnDamage(DestructibleEntity destructible, float amount, Vector2 from);
+    public event OnDamage OnDamageEvent;
 
-    public abstract void Damage(float damage, Vector2 hitPos);
+    public virtual void Damage(float damage) {
+        health -= damage;
+        OnDamageEvent.Invoke(this, damage, position);
+
+    }
+
+    public virtual void Damage(float damage, Vector2 hitPos) {
+        health -= damage;
+        OnDamageEvent.Invoke(this, damage, hitPos);
+    }
 }
